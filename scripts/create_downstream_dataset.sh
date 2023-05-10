2540d7b1add2e11ac115b2a632e7fac4fe87f1c3
kun <qbetterkk@gmail.com>
2023-05-10 19:33:25 -0400

    unomment script for other dataset

--- scripts/create_downstream_dataset.sh
+++ scripts/create_downstream_dataset.sh
@@ -8,35 +8,35 @@ VERSION=0.1.0
 SUBJECT=DialoGLMRedditDataset
 USAGE="Usage: "
 
-# # Please follow parlai to download WoW and WoI dataset
-# cd ../data/
-# WOW_PATH=./wizard_of_wikipedia
-# if [ ! -f wizard_of_wikipedia.tgz ]
-# then
-#     wget http://parl.ai/downloads/wizard_of_wikipedia/wizard_of_wikipedia.tgz
-# fi
-# mkdir -p ${WOW_PATH}
-# if [ ! -d wizard_of_wikipedia ]
-# then
-#     tar -zxvf ./wizard_of_wikipedia.tgz -C ./wizard_of_wikipedia
-# fi
-# cd ../scripts
-# python downstream_tasks_converter.py WoWConverter ../data/wizard_of_wikipedia
+# Please follow parlai to download WoW and WoI dataset
+cd ../data/
+WOW_PATH=./wizard_of_wikipedia
+if [ ! -f wizard_of_wikipedia.tgz ]
+then
+    wget http://parl.ai/downloads/wizard_of_wikipedia/wizard_of_wikipedia.tgz
+fi
+mkdir -p ${WOW_PATH}
+if [ ! -d wizard_of_wikipedia ]
+then
+    tar -zxvf ./wizard_of_wikipedia.tgz -C ./wizard_of_wikipedia
+fi
+cd ../scripts
+python downstream_tasks_converter.py WoWConverter ../data/wizard_of_wikipedia
 
 
-# cd ../data/
-# WOW_PATH=./wizard_of_internet
-# if [ ! -f wizard_of_internet.tgz ]
-# then
-#     wget http://parl.ai/downloads/wizard_of_internet/wizard_of_internet.tgz
-# fi
-# mkdir -p ${WOW_PATH}
-# if [ ! -f wizard_of_internet/train.jsonl ]
-# then
-#     tar -zxvf ./wizard_of_internet.tgz -C ./wizard_of_internet
-# fi
-# cd ../scripts
-# python downstream_tasks_converter.py WoIConverter ../data/wizard_of_internet
+cd ../data/
+WOW_PATH=./wizard_of_internet
+if [ ! -f wizard_of_internet.tgz ]
+then
+    wget http://parl.ai/downloads/wizard_of_internet/wizard_of_internet.tgz
+fi
+mkdir -p ${WOW_PATH}
+if [ ! -f wizard_of_internet/train.jsonl ]
+then
+    tar -zxvf ./wizard_of_internet.tgz -C ./wizard_of_internet
+fi
+cd ../scripts
+python downstream_tasks_converter.py WoIConverter ../data/wizard_of_internet
 
 # Please follow https://github.com/stanfordnlp/coqa-baselines to prepare seq2seq-train-h2 and seq2seq-dev-h2
 if [ ! -d ../data/coqa-baselines ]
